9091ba5ad3a0c2615c2d5fc8b77ec001130dd2aa
Densen <48922583+DensenDavis@users.noreply.github.com>
2022-04-04 15:31:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 # physical_devices = tf.config.list_physical_devices('GPU') 
 # tf.config.experimental.set_memory_growth(physical_devices[0], True)
